3941c824fa0c7bd08a2590c0a12c073397fc2502
0ditya <am827@snu.edu.in>
2024-01-29 16:13:39 +0530

    test

--- Untitled-1.py
+++ Untitled-1.py
@@ -6,36 +6,27 @@ class Employee:
         self.salary = salary
 
 
-employee1 = Employee(emp_id="161E90", name="Ramu", age=35, salary=59000)
-employee2 = Employee(emp_id="171E22", name="Tejas", age=30, salary=82100)
-employee3 = Employee(emp_id="171G55", name="Abhi", age=25, salary=100000)
-employee4 = Employee(emp_id="151K46", name="Jaya", age=32, salary=85000)
+Empa = Employee(emp_id="161E90", name="Ramu", age=35, salary=59000)
+Empb = Employee(emp_id="171E22", name="Tejas", age=30, salary=82100)
+Empc = Employee(emp_id="171G55", name="Abhi", age=25, salary=100000)
+Empd = Employee(emp_id="151K46", name="Jaya", age=32, salary=85000)
 
-print("Employee ID:", employee1.emp_id)
-print("Name:", employee1.name)
-print("Age:", employee1.age)
-print("Salary (PM):", employee1.salary)
-
-employees = [employee1, employee2, employee3, employee4]
+Emps = [Empa, Empb, Empc, Empd]
 
 
-print("press 1 to sort by age")
-print("press 2 to sort by name")
-print("press 3 to sort by salary")
-x=int(input("enter here"))
-if x == 1:
-    sorted_employees = sorted(employees, key=lambda emp: emp.age1)
-elif x == 2:
-    sorted_employees = sorted(employees, key=lambda emp: emp.name)
-elif x == 3:
-    sorted_employees = sorted(employees, key=lambda emp: emp.salary)
-else:
-    print("Invalid input")
+print("1.Age")
+print("2.Name")
+print("3.Salary")
+F=int(input(""))
+if F == 1:
+    sort = sorted(Emps, key=lambda emp: emp.age)
+elif F == 2:
+    sort = sorted(Emps, key=lambda emp: emp.name)
+elif F == 3:
+    sort = sorted(Emps, key=lambda emp: emp.salary)
 
-# Print sorted employees
-for emp in sorted_employees:
-    print("Employee ID:", emp.emp_id)
-    print("Name:", emp.name)
-    print("Age:", emp.age)
-    print("Salary (PM):", emp.salary)
-    print()
+for i in sort:
+    print("employee id:", i.emp_id)
+    print(" name:", i.name)
+    print(" age:", i.age)
+    print("salary (pm):", i.salary)
